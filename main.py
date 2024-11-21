6944b32d5facca58f7e56e9c4f3f6df50b3634ef
PhamQuang <PhamQuang@activants.com>
2024-11-21 17:23:06 +0800

    Complete extraction

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import time
+import pandas as pd
+
 from utils import *
 
 # Set up the Selenium WebDriver (make sure to specify the path to your WebDriver)
@@ -15,23 +17,35 @@ driver = webdriver.Chrome(service=service, options=options)
 # Allow some time for the page to load
 time.sleep(1)  # Adjust sleep time as necessary
 
-symbol = "FPT".upper()
-url = get_HOSE_url(symbol, "", "trailing")
-driver.get(url)
+income_metrics = ['Revenue', 'Gross Profit', 'Operating Income', 'Net Income', 'EPS (Diluted)', 'Operating Margin', 'Free Cash Flow']
+balance = []
+cashflow = []
 
-values = get_values_by_metric(driver, "Revenue", switch_order=True, format="b")
+symbols = ["FPT", "pdn"]
+period_type = 'trailing' # trailing
+
+report_path = 'output/Companies Reports.xlsx'
+writer = pd.ExcelWriter(report_path, engine = 'xlsxwriter')
+
+# Income statements
+for sym in symbols:
+	income_statement = []
+
+	sym = sym.upper()
+	url = build_HOSE_url(sym, '', period_type)
+	driver.get(url)
+
+	navigate_report(driver)
+
+	period_name = "Fiscal Year" if period_type == '' else 'Fiscal Quarter'
+	period_col_name = get_values_by_metric(driver, period_name)
+
+	for metric in income_metrics:
+		values = get_values_by_metric(driver, metric)
+		income_statement.append(values)
+ 
+	df = pd.DataFrame(income_statement, columns=period_col_name)
+	df.to_excel(writer, sheet_name = f"{sym}", index=False)
+
+writer.close()
 driver.quit()
-
-# try:
-#     # Locate the revenue element on the page
-#     metric = "Revenue"
-#     table_element = driver.find_element(By.XPATH, '//*[@id="main-table"]/tbody')
-    
-#     child_title_element = driver.find_element(By.XPATH, f"//a[text()='{metric}']")
-#     parent_row = child_title_element.find_element(By.XPATH, "./ancestor::tr")
-
-# except Exception as e:
-#     print(f'Error occurred: {e}')
-# finally:
-#     # Close the browser
-#     driver.quit()
--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.common.by import By
 from web_extraction import *
+import re
 
-def get_HOSE_url(symbol, report_type="", period="Quarterly"):
+def build_HOSE_url(symbol, report_type="", period="Quarterly"):
 	
 	assert report_type in ["", "balance-sheet","cash-flow-statement","ratios"], \
 			"Invalid value for 'report_type'. Must be '', 'balance-sheet','cash-flow-statement','ratios'."
@@ -13,37 +14,48 @@ def get_HOSE_url(symbol, report_type="", period="Quarterly"):
 
 	return url
 
-def get_values_by_metric(driver, metric, switch_order=True, format="b"):
-	assert format in ["", "b"], \
-		"Invalid value for 'report_type'. Must be '', 'b'."
+def navigate_report(driver):
+	# Reverse the order (oldest - newest)
+	switch_element = driver.find_element(By.XPATH, '//button[@title="Switch order of columns"]')
+	if 'active' not in switch_element.get_attribute('class'):
+		click_element(driver, '//button[@title="Switch order of columns"]')
 	
+	# Change to Bilions 
+	click_element(driver, '//button[@title="Change number units"]')
+	click_element(driver, '//button[contains(text(), "Billions")]')
+
+def get_values_by_metric(driver, metric):
+	element = 'a' if metric=='Revenue' else 'div'
 	try:
-		if switch_order:
-			switch_element = driver.find_element(By.XPATH, '//button[@title="Switch order of columns"]')
-			if 'active' not in switch_element.get_attribute('class'):
-				click_element(driver, '//button[@title="Switch order of columns"]')
-			print(switch_element.get_attribute('class'))
-		if format == 'b':
-			click_element(driver, '//button[@title="Change number units"]')
-			click_element(driver, '//button[contains(text(), "Billions")]')
-
-		# switch_order
-		# class contains `active`, eg `controls-btn sm:py-2 active`
-
 		table_element = driver.find_element(By.XPATH, '//*[@id="main-table"]/tbody')
 		
-		child_title_element = table_element.find_element(By.XPATH, f"//a[text()='{metric}']")
+		child_title_element = table_element.find_element(By.XPATH, f"//{element}[text()='{metric}']")
 		parent_row = child_title_element.find_element(By.XPATH, "./ancestor::tr")
+		if 'Fiscal Quarter' in metric:
+			return process_quarter(parent_row.text)
+		
 		return process_row(parent_row.text)
 
 	except Exception as e:
 		print(f'Error occurred: {e}')
 		return ""
 
+def process_quarter(text):
+	text_seq = text.split("\n")
+	title = text_seq[0]
+	values = re.findall(r'Q\d \d{4}', text_seq[1])
+
+	# Use regex to find all occurrences of the pattern 'Q[number] [year]'
+	return [title] + values
+
 def process_row(text):
 	# 'Revenue\n59,931,389 57,790,313 55,029,450 52,617,901 50,962,143 48,348,957 45,960,653 44,009,528 41,678,929 39,255,542 37,801,189 35,657,263 33,619,912 32,447,868 30,786,163 29,830,401 29,283,884 28,835,129 28,681,045 27,716,960\nUpgrade'
 	text_seq = text.split("\n")
 	title = text_seq[0]
 	values = text_seq[1].split(" ")
-	return "\t".join([title] + values)
+	
+	values = [v.replace(',','') for v in values] # remove comma
+	values = [v.replace('%','') for v in values] # remove %
+	values = [float(v) for v in values] # convert to float
+	return [title] + values
 	
